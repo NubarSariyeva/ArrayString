45d5416245d507e4b0967dcafd326df220cbd0ea
NubarSariyeva <127185998+NubarSariyeva@users.noreply.github.com>
2023-04-27 08:56:22 +0400

    Add files via upload

---
+++ Array & String/assets/js/main.js
@@ -0,0 +1,9 @@
+alert()
+let text = ["Nubar","Ceyran","Yunis","Pakize"];
+let count=0;
+for (let i = 0; i < text.length; i++) {
+    if (text[i].includes("n")){
+        count++;
+}
+}
+console.log(count);
